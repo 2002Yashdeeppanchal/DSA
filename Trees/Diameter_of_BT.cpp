a2901959af67419ac6048272bf2f4c5b9a1b326a
2002Yashdeeppanchal <67218413+2002Yashdeeppanchal@users.noreply.github.com>
2022-10-03 17:24:41 +0530

    Update Diameter_of_BT.cpp

--- Trees/Diameter_of_BT.cpp
+++ Trees/Diameter_of_BT.cpp
@@ -1,9 +1,12 @@
-O(N^2) sol can be as we traversal all the nodes and while traversing we find maxDepth on its left as well as maxDepth on its right (we are considering this node as the curving point of the path) and update the longestpath by lh+rh   and do this for all the nodes after that longestpath stores the ans
+O(N^2) sol can be as we traversal all the nodes and while traversing we find maxDepth on its left as well as maxDepth on its 
+right (we are considering this node as the curving point of the path) and update the longestpath by lh+rh   and do this for 
+all the nodes after that longestpath stores the ans
 
 
 
 
-but think why we cal the left heigh and right height every time we simply update the maxDepth code and hence we find the ans while finding the maxDepth so its a O(N) sol
+but think why we cal the left heigh and right height every time we simply update the maxDepth code and hence we find 
+the ans while finding the maxDepth so its a O(N) sol
 
   int maxDepth(TreeNode* root,int& diameter){
         if(root==NULL){
