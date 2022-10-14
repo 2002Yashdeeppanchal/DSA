c293a0e65d26a954e6361890c21269db96c3713c
2002Yashdeeppanchal <67218413+2002Yashdeeppanchal@users.noreply.github.com>
2022-10-14 15:59:37 +0530

    Create maxheap and minheap as priority queue.cpp

---
+++ heaps/maxheap and minheap as priority queue.cpp
@@ -0,0 +1,15 @@
+void solve(){   
+    
+    priority_queue<int>maxheap;
+
+    maxheap.push(9);
+    maxheap.push(6);
+    maxheap.push(4);
+    maxheap.push(3);
+    maxheap.push(2);
+
+    cout<<maxheap.top()<<endl;
+
+    priority_queue<int,vector<int>,greater<int>>minheap;
+    
+}   
