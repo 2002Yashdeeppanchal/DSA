6c7f8c6a49848f1c04bcf1580c3b1eb94e9381f5
2002Yashdeeppanchal <67218413+2002Yashdeeppanchal@users.noreply.github.com>
2022-12-24 12:26:22 +0530

    Create subsequence with max sum with non adjacent elements.cpp

---
+++ Dynamic Programming/subsequence with max sum with non adjacent elements.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+int f(int ind,vector<int> &nums,vector<int> &dp){
+    if(ind==0)return nums[0];
+    if(ind<0)return 0;
+    
+    if(dp[ind]!=-1)return dp[ind];
+    
+    int pick=nums[ind] + f(ind-2,nums,dp);
+    
+    int notpick=0 + f(ind-1,nums,dp);
+    
+    return dp[ind]=max(pick,notpick);
+}
+int maximumNonAdjacentSum(vector<int> &nums){
+    //memoisation
+    int n=nums.size();
+    vector<int>dp(n,-1);
+    return f(n-1,nums,dp);
+    
+    //tabulation
+    vector<int>dp(n,-1);
+    dp[0]=nums[0];
+    int neg=0;
+    for(int i=1;i<n;i++){
+        int pick=nums[i];
+        if(i>1){
+            pick+=dp[i-2];
+        }
+        int notpick=0+dp[i-1];
+        dp[i]=max(pick,notpick);
+    }
+    return dp[n-1];
+    
+    
+    //space optimisation
+    int prev=nums[0];
+    int prev2=0;
+    for(int i=1;i<n;i++){
+        int pick=nums[i];
+        if(i>1){
+            pick+=prev2;
+        }
+        int notpick=0+prev;
+        int curr=max(pick,notpick);
+        prev2=prev;
+        prev=curr;
+    }
+    return prev;
+    
+}
