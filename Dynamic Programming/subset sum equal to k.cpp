398166ad16dcb43104d4bbe4b7ccabe5f59c3fd7
2002Yashdeeppanchal <67218413+2002Yashdeeppanchal@users.noreply.github.com>
2022-12-26 17:30:11 +0530

    Update subset sum equal to k.cpp

--- Dynamic Programming/subset sum equal to k.cpp
+++ Dynamic Programming/subset sum equal to k.cpp
@@ -1,3 +1,5 @@
+for recursion going n-1 to 0
+
 #include <bits/stdc++.h>
 
 bool f(int ind,int target,vector<int> &arr,vector<vector<int>> &dp){
@@ -33,4 +35,45 @@ bool subsetSumToK(int n, int k, vector<int> &arr) {
     return dp[n-1][k];
     
     
+}
+
+
+for recursion going 0  -  n-1
+which i master in recursion playlist
+    
+    
+    
+#include <bits/stdc++.h>
+
+bool f(int ind,int target,vector<int> &arr,vector<vector<int>> &dp,int n){
+    if(ind>=n)return (target==0);
+    if(dp[ind][target]!=-1)return dp[ind][target];
+    bool nottake=f(ind+1,target,arr,dp,n);
+    bool take=false;
+    if(arr[ind]<=target){
+        take=f(ind+1,target-arr[ind],arr,dp,n);
+    }
+    return dp[ind][target] = take or nottake;
+}
+
+bool subsetSumToK(int n, int k, vector<int> &arr) {
+    vector<vector<bool>>dp(n,vector<bool>(k+1,false));
+//     return f(0,k,arr,dp,n);
+    
+    //tabulation
+    for(int i=0;i<n;i++){
+        dp[i][0]=true;
+    }
+    dp[n-1][arr[n-1]]=true;
+    for(int ind=n-2;ind>=0;ind--){
+        for(int target=1;target<=k;target++){
+            bool nottake=dp[ind+1][target];
+            bool take=false;
+            if(arr[ind]<=target){
+                take=dp[ind+1][target-arr[ind]];
+            }
+            dp[ind][target] = take or nottake;
+         }
+    }
+    return dp[0][k];
 }
