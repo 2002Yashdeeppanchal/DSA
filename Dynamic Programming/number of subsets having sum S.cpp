81d476fd73e99e6b1d9992c66da63db54aedb745
2002Yashdeeppanchal <67218413+2002Yashdeeppanchal@users.noreply.github.com>
2022-12-27 15:36:00 +0530

    Create number of subsets having sum S.cpp

---
+++ Dynamic Programming/number of subsets having sum S.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h> 
+
+int f(int ind,int S,vector<int> &num,vector<vector<int>> &dp){
+    if(ind==0){
+        if(S==0 and num[0]==0)return 2;
+        if(S==0 or S==num[0])return 1;
+        return 0;
+    }
+    if(dp[ind][S]!=-1)return dp[ind][S];
+    int notpick=f(ind-1,S,num,dp);
+    int pick=0;
+    if(num[ind]<=S)pick=f(ind-1,S-num[ind],num,dp);
+    
+    return dp[ind][S] = pick + notpick;
+}
+
+int findWays(vector<int> &num, int tar)
+{
+    int n=num.size();
+    vector<vector<int>>dp(n,vector<int>(tar+1,0));
+//     return f(n-1,tar,num,dp);
+    
+    //tabulation
+    dp[0][0]=1;
+    dp[0][num[0]]=1;
+    if(num[0]==0)dp[0][0]=2;
+    
+    for(int ind=1;ind<n;ind++){
+        for(int sum=0;sum<=tar;sum++){
+            int notpick=dp[ind-1][sum];
+            int pick=0;
+            if(num[ind]<=sum)pick=dp[ind-1][sum-num[ind]];
+            dp[ind][sum] = pick + notpick;
+        }
+    }
+    return dp[n-1][tar];
+    
+    //space optimisation
+//     vector<int>prev(tar+1,0),curr(tar+1);
+//     prev[0]=1;
+//     curr[0]=1;
+//     if(num[0]<=tar)prev[num[0]]=1;
+    
+//     for(int ind=1;ind<n;ind++){
+//         for(int sum=0;sum<=tar;sum++){
+//             int notpick=prev[sum];
+//             int pick=0;
+//             if(num[ind]<=sum)pick=prev[sum-num[ind]];
+
+//             curr[sum] = pick + notpick;
+//         }
+//         prev=curr;
+//     }
+//     return prev[tar];
+}
